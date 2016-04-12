6d8ed6b3f6007f28af3b10cffae452b8c989948a
Daisy Aurora <daurora@ucsd.edu>
2016-04-11 18:38:02 -0700

    initial commit

---
+++ assignment1_p1.py
@@ -0,0 +1,20 @@
+__author__ = ’ daurora@ucsd.edu , A99407185 , student2@ucsd.edu , student2PID ’
+
+def getPossibleActions(currentPrime):
+	# This method would return the l i s t of prime
+	# numbers reachable from current prime .
+	# Note − this should not include the prime numbers
+	# which have already been processed , either in the
+	# frontier or in the closed list .
+	return listOfPrimes
+
+def getPath(startingPrime ,finalPrime): 
+	# your code here
+	return path
+
+def main():
+	primes=str(sys.stdin.readline()).split() 
+	print(getPath(primes[0] ,primes[1]))
+
+if __name__ == ’__main__’: 
+	main()
