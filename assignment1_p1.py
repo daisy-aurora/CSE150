da2b11e4bef3c2a7482f65aee81a48ea29b87ad3
Daisy Aurora <daurora@ucsd.edu>
2016-04-11 18:55:28 -0700

    checkprime function

--- assignment1_p1.py
+++ assignment1_p1.py
@@ -1,5 +1,17 @@
 __author__ = ’ daurora@ucsd.edu , A99407185 , student2@ucsd.edu , student2PID ’
 
+def checkPrime(input):
+	if input < 2:
+		return False
+	if input == 2:
+		return True
+	input *= 1.0
+
+	for divisor in range (2, int(input**0.5)+1):
+		if input % divisor == 0:
+			return False
+	return True
+
 def getPossibleActions(currentPrime):
 	# This method would return the l i s t of prime
 	# numbers reachable from current prime .
