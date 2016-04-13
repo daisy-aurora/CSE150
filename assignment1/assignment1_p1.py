8b84abf476b9797af8b237fd9e105e119a3a50f7
Daisy Aurora <daurora@ucsd.edu>
2016-04-13 09:40:37 -0700

    finished p1

--- assignment1/assignment1_p1.py
+++ assignment1/assignment1_p1.py
@@ -3,12 +3,13 @@ import sys
 import Queue
 
 allPrime = []
+visited = []
 path = []
 potential = set()
 
 class Node:
-    def __init__(self, val):
-        self.parent  = None
+    def __init__(self, parent, val):
+        self.parent = parent
         self.val = val
 
 def checkPrime(num):
@@ -36,19 +37,25 @@ def getPossibleActions(currentPrime):
     for prime in allPrime:
         primeList = list(map(int, str(prime)))
         differences = 0
-
+        
         for i in range(0 , len(currPrimeList)):
             if currPrimeList[i] != primeList[i]:
                 differences += 1
 
         if differences == 1:
-            listOfPrimes.append(prime)
+            isVisited = False
+            for visitedNumber in visited:
+                if visitedNumber == prime:
+                    isVisited = True
+            if isVisited == False:
+                listOfPrimes.append(prime)
+                visited.append(prime)
 
     return listOfPrimes
 
 def getPath(startingPrime ,finalPrime):
     # your code here
-    root = Node(startingPrime)
+    root = Node(None, startingPrime)
     q = Queue.Queue()
     q.put(root)
 
@@ -56,24 +63,52 @@ def getPath(startingPrime ,finalPrime):
         path.append(root.val)
         return path
     while not q.empty():
+        # CODE TEST HERE
+#         for elem in list(q.queue):
+#             print(elem.val), 
+#             print("parent: "),
+#             if elem.parent == None:
+#                 print("None"),
+#             else:
+#                 print(elem.parent.val),
+#             print(","),
+#         print
+        # END OF CODE TEST
         cur = q.get()
-        potential.add(cur.value)
+        #potential.add(cur.val)
+        if cur.val == finalPrime:
+            temp = cur
+            while temp.parent is not None:
+                path.append(temp.val)
+                temp = temp.parent
+            
+            #append the staring prime
+            path.append(temp.val)
+            
+            #reverse the list
+            path.reverse()
+            
+            #convert the list to string
+            result = ""
+            for x in path:
+                result+= str(x) + " "
+            return(result)
+            
         pList = getPossibleActions(cur.val)
         for n in pList:
-            if n not in potential:
-                nextNode = Node(n, cur)
-                if n == finalPrime:
-                    temp = cur
-                    while temp.parent != None:
-                        path.append(temp.val)
-                        temp = cur.parent
-                    return path.reverse()
-                q.put(nextNode)
+            #if n not in potential:
+            nextNode = Node(cur, n)
+            q.put(nextNode)
 
-    print("UNSOLVABLE")
-    return path
+    return("UNSOLVABLE")
 
 def getAllPrimes(startingPrime, finalPrime):
+    startingPrimeLength = len(startingPrime)
+    finalPrimeLength = len(finalPrime)
+    
+    if startingPrimeLength != finalPrimeLength:
+        return
+    
     for x in range(int(startingPrime), int(finalPrime)):
         checkPrimeResult = checkPrime(x)
         if checkPrimeResult == True:
@@ -83,14 +118,15 @@ def getAllPrimes(startingPrime, finalPrime):
 def main():
     primes=str(sys.stdin.readline()).split()
     getAllPrimes(primes[0] ,primes[1])
-
+    visited.append(int(primes[0]))
+    
     # THIS PART IS FOR TESTING PURPOSE
-    for p in allPrime:
-        print(p),
-        lists = getPossibleActions(p)
-        for x in lists:
-            print(x),
-        print
+    #for p in allPrime:
+    #    print(p),
+    #    lists = getPossibleActions(p)
+    #    for x in lists:
+    #        print(x),
+    #    print
     # END OF TESTING CODE
 
     print(getPath(primes[0] ,primes[1]))
