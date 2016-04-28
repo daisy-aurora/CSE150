148de55065c8e47108e0982eba7daccfd30e4fd5
msarwo <msarwo@ucsd.edu>
2016-04-27 18:08:44 -0700

    Error: global 'bestAction' not define

--- assignments2/solutions/p1_minimax_player.py
+++ assignments2/solutions/p1_minimax_player.py
@@ -8,6 +8,60 @@ class MinimaxPlayer(Player):
     def __init__(self):
         self.cache ={}
 
+    player = None
+    bestAction = None
+
+    def minimax(self, state, moveNum):
+        index = state.M * 2 + 2
+        actions = state.actions()
+
+        if state.is_terminal():
+            return state.utility(player)
+        elif player.row == state.player_row:
+            utility = max(state, moveNum, actions)
+        else:
+            utility = min(state, moveNum, actions)
+
+        return utility
+
+    def max(self, state, moveNum, actions):
+        v = -999
+
+        if not(actions):
+            nextState = State(state.board, state.opponent_row, state.player)
+            v = self.minimax(nextState, moveNum + 1)
+
+        while (actions):
+            curAction = actions.pop()
+            nextState = state.result(curAction)
+            tempV = self.minimax(nextState, moveNum + 1)
+
+            if (v <= tempV):
+                v = tempV
+                if (moveNum == 1):
+                    bestAction = curAction
+                    index = curAction.index
+        return v
+
+    def min(self, state, moveNum, actions):
+        v = 999
+
+        if not(actions):
+            nextState = State(state.board, state.opponent_row, state.player)
+            v = self.minimax(nextState, moveNum + 1)
+
+        while (actions):
+            curAction = actions.pop(0)
+            nextState = state.result(curAction)
+            tempV = self.minimax(nextState, moveNum + 1)
+
+            if (v > tempV):
+                v = tempV
+                if (moveNum == 1):
+                    bestAction = curAction
+                    index = curAction.index
+        return v
+
     def move(self, state):
         """
         Calculates the best move from the given board using the minimax
@@ -15,4 +69,9 @@ class MinimaxPlayer(Player):
         :param state: State, the current state of the board.
         :return: Action, the next move
         """
-        raise NotImplementedError("Need to implement this method")
+        global player, bestAction
+        player = state.player
+        utility = self.minimax(state, 1)
+
+        return bestAction
+        #raise NotImplementedError("Need to implement this method")
